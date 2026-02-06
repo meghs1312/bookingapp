727d13034329ffcf47841d7e293347ecd4368382
Meghana Gugulothu <meghanagugulothu2004@gmail.com>
2026-02-06 10:23:13 +0530

    production api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://bookingapp-iah9.onrender.com/api"
 });
 
 export default instance;
